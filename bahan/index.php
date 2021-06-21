2668b43475eec866f7b62d15b0853ab8490d6329
arsy12 <58380858+arsy12@users.noreply.github.com>
2021-06-21 12:56:51 +0700

    Add files via upload

---
+++ bahan/index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Transparent Login From</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+       <div class="loginBox" >
+           <img src="/bahan/img/hay.png" class="user">
+              <h2>Log In Here</h2>
+              <form>
+                  <p>Email</p>
+                  <input type="text" name="" placeholder="Enter Email">
+                  <p>password</p>
+                  <input type="password" name="" placeholder="......">
+                  <input type="submit" name="" value="Sign In">
+                  <a herf="#">Forget Password<a>
+             </form>
+       </div>
+    </body>
+</html>
